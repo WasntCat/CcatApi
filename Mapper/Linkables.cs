df851aaa34a8b6b252612b0f3ba73694816f4a12
Cat <104047478+WasntCat@users.noreply.github.com>
2023-10-17 10:33:01 -0400

    Console SRC

---
+++ Mapper/Linkables.cs
@@ -0,0 +1,22 @@
+﻿
+namespace CcatApi.Mapper
+{
+    internal class Linkables
+    {
+        // Can not Use the Format Display Function with RNG Type since it would through a error since theres noting
+        //internal static string CatLink = "https://api.thecatapi.com/v1/images/search"; // For RNG
+
+        // internal static string CatLink = "https://api.thecatapi.com/v1/images/search?mime_types=png"; // For Pngs
+        // internal static string CatLink = "https://api.thecatapi.com/v1/images/search?mime_types=jpg"; // For Jpg
+        internal static string CatLink = "https://api.thecatapi.com/v1/images/search?mime_types=gif"; // For Gifs
+
+        // For Displaying the Format type
+        internal static string SerType = CatLink.Split('=')[1];
+
+        // The Discord Webhook that will be embeding the Cats
+        internal static string DiscordWebh = " WEBHOOK HERE";
+
+        // Why not just put this here p-p
+        internal static int SleepTime = 15000;
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,93 @@
+﻿using CcatApi.Mapper;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Net.Http;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace CcatApi
+{
+    internal class Program
+    {
+
+        static async Task Main(string[] args)
+        {
+            using (HttpClient htpc = new HttpClient())
+            {
+                while (true)
+                {
+                    try
+                    {
+                        string Curl = await RequestCat(htpc, Linkables.CatLink);
+                        await PostDisc(Linkables.DiscordWebh, Curl);
+
+                        Console.ForegroundColor = ConsoleColor.DarkGray;
+
+                        Console.WriteLine(" ");
+                        Console.WriteLine(" [ = ]  ==========================  [ = ]");
+                        Console.WriteLine($" [ CAT ] Request Posted, Sent to webhook, Sleeping for {Linkables.SleepTime}");
+                        Console.WriteLine($" [ FORMAT ] Pulled Format Type, {Linkables.SerType}"); // Hash out if using RNG Search
+                        Console.WriteLine($" [ WEBHOOK ] Sent Location, {Linkables.DiscordWebh}");
+                        Console.WriteLine(" [ = ]  ==========================  [ = ]");
+                        Console.WriteLine(" ");
+
+                        Thread.Sleep(Linkables.SleepTime); // Sleeping For a good 15s to ensure to not get Ratelimited
+                    }
+                    catch (Exception cats)
+                    {
+                        Console.ForegroundColor = ConsoleColor.DarkRed;
+                        Console.WriteLine($" [ MEOW ] Internal Error : {cats}");
+                    }
+                }
+            }
+        }
+
+
+
+        #region Post Embed [ ## Discord Webhook] , Request Cats [ ## From catapi]
+        static async Task<string> RequestCat(HttpClient htpc, string CatRQ)
+        {
+            HttpResponseMessage resp = await htpc.GetAsync(CatRQ);
+
+            if (resp.IsSuccessStatusCode)
+            {
+                string con = await resp.Content.ReadAsStringAsync();
+                JArray jsr = JArray.Parse(con);
+                string Catimg = jsr[0]["url"].ToString();
+                return Catimg;
+            }
+            else
+            {
+                Console.ForegroundColor = ConsoleColor.DarkRed;
+                throw new Exception($" [ MEOW ] Internal Error, API CODE : {resp.StatusCode}");
+            }
+        }
+        static async Task PostDisc(string Dwebhok, string CatRQ)
+        {
+            using (HttpClient htpc = new HttpClient())
+            {
+
+                var payl = new
+                {
+                    content = "",
+                    embeds = new[]
+                    {
+                    new
+                    {
+                        image = new
+                        {
+                            url = CatRQ
+                        }
+                    }
+                }
+                };
+
+                string jsp = Newtonsoft.Json.JsonConvert.SerializeObject(payl);
+                var con = new StringContent(jsp, System.Text.Encoding.UTF8, "application/json");
+                await htpc.PostAsync(Dwebhok, con);
+            }
+        }
+        #endregion
+
+    }
+}
